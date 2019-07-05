b626b8f37f41b12d0d89b85c3d49b91de42aabc5
Alessandro Confetti <alessandro.confetti@zigolab.it>
2019-07-05 17:21:08 +0200

    chore(make): exclude config folder from check_from_secrets, remove node_modules during dev_clean_up

--- .make/development/cleanup_local_data.sh
+++ .make/development/cleanup_local_data.sh
@@ -17,6 +17,13 @@ fi
 -f "docker-compose.local-dev.yml" \
 -o "-v"
 
+read -e -p "Do you also want to remove node modules folder? [Y/n] " response;
+if [[ ${response} == y ]]; then
+    echo "Removing all installed node modules..."
+    rm -rf node_modules
+    echo "...done"
+fi
+
 read -e -p "Do you also want to remove all unused containers, volumes, networks and images (both dangling and unreferenced)? [y/N] " response;
 if [[ ${response} == y ]]; then
     echo "Removing all unused containers, volumes, networks and images..."
